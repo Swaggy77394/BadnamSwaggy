4bae6af2c680468e9fc0a8ab56480ad5029fb911
Badnam465 <kritikumari8665@gmail.com>
2025-11-21 13:45:04 +0530

    Update play.py

--- SONALI_MUSIC/utils/inline/play.py
+++ SONALI_MUSIC/utils/inline/play.py
@@ -1,14 +1,6 @@
-# ======================================================
-# ©️ 2025-26 All Rights Reserved by Revange 😎
-
-# 🧑‍💻 Developer : t.me/dmcatelegram
-# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
-# 📢 Telegram channel : t.me/dmcatelegram
-# =======================================================
-
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
-from pyrogram.types import InlineKeyboardButton, WebAppInfo
+from pyrogram.types import InlineKeyboardButton
 from SONALI_MUSIC import app
 import config
 from SONALI_MUSIC.utils.formatters import time_to_seconds
@@ -42,47 +34,40 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "🅚︎—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—🅡︎————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——🅘︎———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———🅣︎——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————🅘︎—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————🅑︎————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————🅞︎———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————🅣︎——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————🅢︎—"
     else:
-        bar = "—————————◉"
+        bar = "🅚︎—————————"
     buttons = [
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+                [
+         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
-         [
-             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20"),
-             InlineKeyboardButton(text="ᴄʜᴧᴛ", url=f"https://t.me/lll_BADNAM_BABY_lll"),
-             InlineKeyboardButton(text="𝟤𝟢 s + >", callback_data="seek_forward_20")
-         ],
         [
-            InlineKeyboardButton(text="✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true"),
-        ]
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/kriti_bot_update"),
+         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/KRITI_SUPPORT_GROUP"),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -90,23 +75,15 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-         ],
-        [
-             InlineKeyboardButton(text="< - 𝟤𝟢 s", callback_data="seek_backward_20"),
-             InlineKeyboardButton(text="ᴄʜᴧᴛ", url=f"https://t.me/lll_BADNAM_BABY_lll"),
-             InlineKeyboardButton(text="𝟤𝟢 s+ >", callback_data="seek_forward_20")
-         ],
-        [
-            InlineKeyboardButton(text="✙ ʌᴅᴅ ϻє ɪη ʏσυʀ ɢʀσυᴘ ✙", url=f"https://t.me/{app.username}?startgroup=true"),
-        ]
+         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/kriti_bot_update"),
+         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/KRITI_SUPPORT_GROUP"),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
-    def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+
+
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
     buttons = [
         [
             InlineKeyboardButton(
@@ -126,7 +103,7 @@ def stream_markup(_, chat_id):
         ],
     ]
     return buttons
-                
+
 
 
 def livestream_markup(_, videoid, user_id, mode, channel, fplay):
@@ -176,12 +153,3 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
-
-# ======================================================
-# ©️ 2025-26 All Rights Reserved by Revange 😎
-
-# 🧑‍💻 Developer : t.me/dmcatelegram
-# 🔗 Source link : https://github.com/hexamusic/REVANGEMUSIC
-# 📢 Telegram channel : t.me/dmcatelegram
-# =======================================================
-    
